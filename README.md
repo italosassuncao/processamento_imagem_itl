623e04463be65d9e8f127f5ae9968e7cea271565
italosassuncao <italo18sil@gmail.com>
2023-11-14 18:48:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# package_name
+
+__Description:__   
+The package image-processing is used to:  
+	Process images using histogram matching and structural similarity in processing, also allowing image resizing.  
+	The utilities package reads the images, saves them and plots their results
+	-
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+
+```bash
+pip install image_processing
+```
+
+## Author
+Italo
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requisitos.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processing_package",
+    version="0.0.1",
+    autor="Italo",
+    email="italo18sil@gmail.com",
+    descricao="Vazio",
+    descricao_longa=page_description,
+    descricao_longa_tipo_conteudo="text/markdown",
+    url="",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.6"
+)
